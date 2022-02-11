0cba40faa2848cdaad92d67ea9ed2b75d283cc78
Jeanet Lin <linett@gmail.com>
2022-02-11 10:28:17 +0100

    iteration 1

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,3 +1,4 @@
+//this creates the characterAPI inside index.js
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,8 +1,9 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
+  //the fetch all button
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
